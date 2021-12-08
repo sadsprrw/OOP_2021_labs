21cc33dc0fb36e60fc8bf41ae7bd0b3aa589aa1a
Eugen Vorobiov <jeka.vorobjov@gmail.com>
2021-12-08 17:21:05 +0200

    Update DeviceHandler.java

--- lab2/src/main/java/parsers/DeviceHandler.java
+++ lab2/src/main/java/parsers/DeviceHandler.java
@@ -52,80 +52,39 @@ public class DeviceHandler extends DefaultHandler {
     @Override
     public void endElement(String uri, String localName, String qName) {
         switch (qName) {
-            case XMLElements.ID:
-                getLastDevice().setId(elementValue);
-                break;
-            case XMLElements.NAME:
-                getLastDevice().setName(elementValue);
-                break;
-            case XMLElements.ORIGIN:
-                getLastDevice().setOrigin(elementValue);
-                break;
-            case XMLElements.PRICE:
-                getLastDevice().setPrice(Integer.valueOf(elementValue));
-                break;
-            case XMLElements.CRITICAL:
-                getLastDevice().setCritical(Boolean.parseBoolean(elementValue));
-                break;
-            case XMLElements.PERIPHERAL:
-                getLastDevice().getTypes().setPeripherals(Boolean.parseBoolean(elementValue));
-                break;
-            case XMLElements.ENERGYCONSUMPTION:
-                getLastDevice().getTypes().setEnergyConsumption(Short.parseShort(elementValue));
-                break;
-            case XMLElements.COOLER:
-                getLastDevice().getTypes().setCooler(Boolean.parseBoolean(elementValue));
-                break;
-            case XMLElements.GROUP:
-                getLastDevice().getTypes().setGroup(elementValue);
-                break;
-            case XMLElements.PORT:
-                getLastDevice().getTypes().setPort(elementValue);
-                break;
+            case XMLElements.ID -> getLastDevice().setId(elementValue);
+            case XMLElements.NAME -> getLastDevice().setName(elementValue);
+            case XMLElements.ORIGIN -> getLastDevice().setOrigin(elementValue);
+            case XMLElements.PRICE -> getLastDevice().setPrice(Integer.valueOf(elementValue));
+            case XMLElements.CRITICAL -> getLastDevice().setCritical(Boolean.parseBoolean(elementValue));
+            case XMLElements.PERIPHERAL -> getLastDevice().getTypes().setPeripherals(Boolean.parseBoolean(elementValue));
+            case XMLElements.ENERGYCONSUMPTION -> getLastDevice().getTypes().setEnergyConsumption(Short.parseShort(elementValue));
+            case XMLElements.COOLER -> getLastDevice().getTypes().setCooler(Boolean.parseBoolean(elementValue));
+            case XMLElements.GROUP -> getLastDevice().getTypes().setGroup(elementValue);
+            case XMLElements.PORT -> getLastDevice().getTypes().setPort(elementValue);
         }
     }
 
     public void setField(String qName, String content, Map<String ,String> attributes) {
         switch (qName) {
-
-            case XMLElements.DEVICE:
+            case XMLElements.DEVICE -> {
                 Device device = new Device();
                 listOfDevices.add(device);
-                break;
-            case XMLElements.NAME:
-                getLastDevice().setName(content);
-                break;
-            case XMLElements.ID:
-                getLastDevice().setId(content);
-                break;
-            case XMLElements.ORIGIN:
-                getLastDevice().setOrigin(content);
-                break;
-            case XMLElements.PRICE:
-                getLastDevice().setPrice(Integer.valueOf(content));
-                break;
-            case XMLElements.CRITICAL:
-                getLastDevice().setCritical(Boolean.parseBoolean(content));
-                break;
-            case XMLElements.TYPES:
+            }
+            case XMLElements.NAME -> getLastDevice().setName(content);
+            case XMLElements.ID -> getLastDevice().setId(content);
+            case XMLElements.ORIGIN -> getLastDevice().setOrigin(content);
+            case XMLElements.PRICE -> getLastDevice().setPrice(Integer.valueOf(content));
+            case XMLElements.CRITICAL -> getLastDevice().setCritical(Boolean.parseBoolean(content));
+            case XMLElements.TYPES -> {
                 Types types = new Types();
                 getLastDevice().setTypes(types);
-                break;
-            case XMLElements.PERIPHERAL:
-                getLastDevice().getTypes().setPeripherals(Boolean.parseBoolean(content));
-                break;
-            case XMLElements.ENERGYCONSUMPTION:
-                getLastDevice().getTypes().setEnergyConsumption(Short.parseShort(content));
-                break;
-            case XMLElements.COOLER:
-                getLastDevice().getTypes().setCooler(Boolean.parseBoolean(content));
-                break;
-            case XMLElements.PORT:
-                getLastDevice().getTypes().setPort(content);
-                break;
-            case XMLElements.GROUP:
-                getLastDevice().getTypes().setGroup(content);
-                break;
+            }
+            case XMLElements.PERIPHERAL -> getLastDevice().getTypes().setPeripherals(Boolean.parseBoolean(content));
+            case XMLElements.ENERGYCONSUMPTION -> getLastDevice().getTypes().setEnergyConsumption(Short.parseShort(content));
+            case XMLElements.COOLER -> getLastDevice().getTypes().setCooler(Boolean.parseBoolean(content));
+            case XMLElements.PORT -> getLastDevice().getTypes().setPort(content);
+            case XMLElements.GROUP -> getLastDevice().getTypes().setGroup(content);
         }
     }
 }
